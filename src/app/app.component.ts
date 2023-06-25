e42b818643d376c6e86461d52518ac621bc43729
JJS <jjsaiyed@gmail.com>
2023-06-25 14:10:57 +0400

    Current price changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  unitPrice = 1.7797;
+  lotsize = 0;
+  pips = 0;
+  sl = 0;
+  unitPrice = 1.9495;
   held;
   currentValue = this.unitPrice * this.held;
 
  changeHeld(heldUnits:number) {
     return heldUnits * this.unitPrice;
+  }
+  calculate() {
+    this.sl = this.pips * this.lotsize /100;
+    console.log(this);
   }
 }
